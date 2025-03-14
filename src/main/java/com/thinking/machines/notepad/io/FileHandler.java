e1f4f8b257e418e7589bcad4df12af1c79f14711
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-03-14 16:37:58 +0530

    feat(FileHandler) : added property for maintaning old data chache for better optimization and performance

--- src/main/java/com/thinking/machines/notepad/io/FileHandler.java
+++ src/main/java/com/thinking/machines/notepad/io/FileHandler.java
@@ -8,6 +8,7 @@ public  class FileHandler
 {
 private final static int LINES_PER_LOAD=1000; // load 1000 lines per batch
 private int lastLineRead=0; // Track last loaded line
+private List<String> cachedLines=new ArrayList<>(); // Cache loaded lines
 private static final List<String> knownExtensions = Arrays.asList("txt", "java", "c", "cpp", "py", "html", "css");
 private String extension;
 private String fileName;
