d9d46bc3c0985e3f35a2fb6a30d832aff10df009
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-03-18 15:35:10 +0530

    feat(Config) :  added Config class

---
+++ src/main/java/com/thinking/machines/notepad/models/Config.java
@@ -0,0 +1,9 @@
+package com.thinking.machines.notepad.models;
+public class Config implements java.io.Serializable
+{
+public boolean wordWrap=false;
+public boolean statusBar=true;
+public boolean matchCase=false;
+public boolean wrapAround=false;
+public String lastSearchedText="";
+}
