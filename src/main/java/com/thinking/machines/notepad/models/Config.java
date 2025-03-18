3adbf6f348a9b11007a2da99dba73a71122af5d3
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-03-18 18:07:24 +0530

    refactord (Config)

--- src/main/java/com/thinking/machines/notepad/models/Config.java
+++ src/main/java/com/thinking/machines/notepad/models/Config.java
@@ -1,9 +1,10 @@
 package com.thinking.machines.notepad.models;
 public class Config implements java.io.Serializable
 {
-public boolean wordWrap=false;
+public boolean wordWrap=true;
 public boolean statusBar=true;
-public boolean matchCase=false;
+public boolean matchCase=true;
 public boolean wrapAround=false;
+public boolean upDirection=false;
 public String lastSearchedText="";
 }
