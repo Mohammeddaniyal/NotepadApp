dcd59f77201139fa1f2f168ae928c990917f1bd6
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-03-19 18:33:11 +0530

    Modularizing (Notepad) : so introduced new classes for modularity

---
+++ src/main/java/com/thinking/machines/notepad/managers/EditMenuManager.java
@@ -0,0 +1,26 @@
+package com.thinking.machines.notepad.managers;
+import com.thinking.machines.notepad.io.*;
+import com.thinking.machines.notepad.models.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class EditMenuManager
+{
+private JTextArea textArea;
+private JMenuBar menuBar; 
+private Config config;
+private FileHandler fileHandler;
+public EditMenuManager(JTextArea textArea,JMenuBar menuBar,Config config,FileHandler fileHandler)
+{
+this.textArea=textArea;
+this.menuBar=menuBar;
+this.config=config;
+this.fileHandler=fileHandler;
+initComponents();
+addEventListeners();
+}
+private void initComponents()
+{}
+private void addEventListeners()
+{}
+}
